fbd0f010c8b399c5c2a8dcecc95a7e12e82560d1
maartenvanels <maarten@ariensvanels.nl>
2025-11-21 15:57:53 +0100

    Fix: Add WiFiClient class to WiFi.h mock

--- test/native/mocks/WiFi.h
+++ test/native/mocks/WiFi.h
@@ -15,4 +15,21 @@ public:
     void disconnect() {}
 };
 
+class WiFiClient : public Client {
+public:
+    WiFiClient() {}
+    int connect(IPAddress ip, uint16_t port) { return 1; }
+    int connect(const char* host, uint16_t port) { return 1; }
+    size_t write(uint8_t b) { return 1; }
+    size_t write(const uint8_t *buf, size_t size) { return size; }
+    int available() { return 0; }
+    int read() { return -1; }
+    int read(uint8_t *buf, size_t size) { return 0; }
+    int peek() { return -1; }
+    void flush() {}
+    void stop() {}
+    uint8_t connected() { return 1; }
+    operator bool() { return true; }
+};
+
 extern WiFiClass WiFi;
